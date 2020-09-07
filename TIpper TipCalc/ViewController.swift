b481773b25dade86e64b965f13a450c07182d3ce
Uzma Kapadia <uzmakap786@gmail.com>
2020-09-06 22:35:55 -0400

    colors and keyboard

--- TIpper TipCalc/ViewController.swift
+++ TIpper TipCalc/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
